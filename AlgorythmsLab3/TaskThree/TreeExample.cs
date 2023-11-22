27d19db36a18068b0c315740404d84951a7fd331
Kango911 <98217481+Kango911@users.noreply.github.com>
2023-11-22 13:53:41 +0500

    Update TreeExample.cs
    K9

--- AlgorythmsLab3/TaskThree/TreeExample.cs
+++ AlgorythmsLab3/TaskThree/TreeExample.cs
@@ -1,13 +1,92 @@
-﻿using AlgLab3.Structures;
+using System;
 
-namespace AlgLab3.TaskThree
+
+class Node
 {
-    public class TreeExample
+    public int Data;
+    public Node Left;
+    public Node Right;
+
+    public Node(int data)
     {
-        public static int Example(Tree root)
+        Data = data;
+        Left = Right = null;
+    }
+}
+
+
+class BinarySearchTree
+{
+    private Node root;
+
+    public BinarySearchTree()
+    {
+        root = null;
+    }
+
+    public void Insert(int data)
+    {
+        root = InsertRec(root, data);
+    }
+
+    private Node InsertRec(Node root, int data)
+    {
+        if (root == null)
         {
-            if (root == null) return 0;
-            return root.Value + Example(root.Left) + Example(root.Right);
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.Data)
+        {
+            root.Left = InsertRec(root.Left, data);
+        }
+        else if (data > root.Data)
+        {
+            root.Right = InsertRec(root.Right, data);
+        }
+
+        return root;
+    }
+
+    public bool Search(int data)
+    {
+        return SearchRec(root, data);
+    }
+
+    private bool SearchRec(Node root, int data)
+    {
+        if (root == null)
+        {
+            return false;
+        }
+
+        if (data == root.Data)
+        {
+            return true;
+        }
+
+        if (data < root.Data)
+        {
+            return SearchRec(root.Left, data);
+        }
+
+        return SearchRec(root.Right, data);
+    }
+
+    public void InOrderTraversal()
+    {
+        InOrderTraversalRec(root);
+        Console.WriteLine();
+    }
+
+    private void InOrderTraversalRec(Node root)
+    {
+        if (root != null)
+        {
+            InOrderTraversalRec(root.Left);
+            Console.Write($"{root.Data} ");
+            InOrderTraversalRec(root.Right);
         }
     }
 }
